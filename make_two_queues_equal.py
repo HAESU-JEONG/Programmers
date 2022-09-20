ca9ba13971ec9e5e5621ff9bb7133a6a7a9d17ea
HAESU-JEONG <junghaesu7@gmail.com>
2022-09-20 15:29:04 +0900

    두 큐 합 같게 만들기

--- make_two_queues_equal.py
+++ make_two_queues_equal.py
@@ -1,48 +1,32 @@
+from collections import deque
+
 def solution(queue1, queue2):
-    sum_queue1 = sum(queue1)
-    sum_queue2 = sum(queue2)
-    total_length = len(queue1)
-    len_queue1 = len(queue1)
+    deq_queue1 = deque(queue1) # convert list to deque
+    deq_queue2 = deque(queue2) # convert list to deque
 
-    sum_total = sum_queue1 + sum_queue2
-    avg = sum_total // 2
+    sum_queue1 = sum(deq_queue1)
+    sum_queue2 = sum(deq_queue2)
 
-    if sum_total % 2 != 0:
+    sum_total = sum(deq_queue1) + sum(deq_queue2)
+
+    if sum_total % 2 != 0: # if sum_total is odd => cannot equal the sum of each queue
         return -1
 
-    elif max(queue1) > avg or max(queue2) > avg:
-                return -1
-
     else:
-        cnt = 0
-        while True:
+        for cnt in range(len(deq_queue1) * 3):
             if sum_queue1 == sum_queue2:
-                break
+                return cnt
 
-            elif sum_queue1 > sum_queue2:
-                if len_queue1 == 0 or len_queue1 == total_length * 2:
-                    return -1
+            if sum_queue1 > sum_queue2:
+                pop_queue1 = deq_queue1.popleft()
+                deq_queue2.append(pop_queue1)
+                sum_queue1 -= pop_queue1
+                sum_queue2 += pop_queue1
 
-                pop_queue1 = queue1.pop(0)
-                queue2.insert(len(queue2), pop_queue1)
-                cnt += 1
-
-                sum_queue1 = sum_queue1 - pop_queue1
-                sum_queue2 = sum_queue2 + pop_queue1
-
-                len_queue1 -= 1
-
-            elif sum_queue1 < sum_queue2:
-                if len_queue1 == 0 or len_queue1 == total_length * 2:
-                    return -1
-
-                pop_queue2 = queue2.pop(0)
-                queue1.insert(len(queue1), pop_queue2)
-                cnt += 1
-
-                sum_queue1 = sum_queue1 + pop_queue2
-                sum_queue2 = sum_queue2 - pop_queue2
-
-                len_queue1 += 1
-
-        return cnt
+            else:
+                pop_queue2 = deq_queue2.popleft()
+                deq_queue1.append(pop_queue2)
+                sum_queue1 += pop_queue2
+                sum_queue2 -= pop_queue2
+        
+        return -1
